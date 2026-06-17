56c6ebbba2d65def899615ee15e044f6c78420cf
Rushika Patel <rushikapatel1119@gmail.com>
2026-06-17 20:35:09 +0530

    Added variables and data types practice

---
+++ variables_and_datatypes.py
@@ -0,0 +1,14 @@
+name = "Lily"
+age = 19
+cgpa = 8.5
+engineering_student = True
+
+print(name)
+print(age)
+print(cgpa)
+print(engineering_student)
+
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(engineering_student))
