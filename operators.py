1566555737e28478ee3a17cc180103d7dde738f9
Rushika Patel <rushikapatel1119@gmail.com>
2026-06-19 22:25:20 +0530

    Added operators practice

---
+++ operators.py
@@ -0,0 +1,34 @@
+# Arithmetic Operators
+
+a = 15
+b = 4
+
+print("Arithmetic Operators")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+print("Power:", a ** b)
+print("Floor Division:", a // b)
+
+print()
+
+# Comparison Operators
+
+print("Comparison Operators")
+print("a == b :", a == b)
+print("a != b :", a != b)
+print("a > b :", a > b)
+print("a < b :", a < b)
+print("a >= b :", a >= b)
+print("a <= b :", a <= b)
+
+print()
+
+# Logical Operators
+
+print("Logical Operators")
+print("a > 10 and b < 5 :", a > 10 and b < 5)
+print("a > 20 or b < 5 :", a > 20 or b < 5)
+print("not(a > b) :", not(a > b))
