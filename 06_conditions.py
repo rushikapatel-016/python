25ab09912f956dee2979f0677c2bffdeb8c6eea9
Rushika Patel <rushikapatel1119@gmail.com>
2026-06-19 23:04:06 +0530

    Added conditional statements practice

---
+++ 06_conditions.py
@@ -0,0 +1,34 @@
+# Conditions in Python
+
+# Example 1: if statement
+
+age = 20
+
+if age >= 18:
+    print("Adult")
+
+print()
+
+# Example 2: if-else statement
+
+number = 7
+
+if number % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
+
+print()
+
+# Example 3: if-elif-else statement
+
+marks = 85
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 70:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("Fail")
