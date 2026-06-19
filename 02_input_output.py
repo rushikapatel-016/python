c3d561d392af26397bad9801b4b5b8f8e2dbdfb2
Rushika Patel <rushikapatel1119@gmail.com>
2026-06-19 23:08:07 +0530

    Update 02_input_output.py

--- 02_input_output.py
+++ 02_input_output.py
@@ -1,7 +1,14 @@
+# Input and Output in Python
+
+# Taking input from the user
+
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 city = input("Enter your city: ")
 
-print("Name:", name)
-print("Age:", age)
-print("City:", city)
+# Displaying output
+
+print("\n----- Student Information -----")
+print("Name :", name)
+print("Age  :", age)
+print("City :", city)
